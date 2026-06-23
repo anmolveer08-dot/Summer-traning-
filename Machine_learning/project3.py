c52cb6f83192ce01ab701f81ff5f5ca841689af2
Anmolveer singh <anmolveer08@gmail.com>
2026-06-23 09:24:41 +0530

    done

---
+++ Machine_learning/project3.py
@@ -0,0 +1,11 @@
+#============================Decision Tree Regression ==============================
+
+import numpy as py
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns 
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error, r2_score
+from sklearn.ensemble import la
